e64ee26e1ad7f2ef35eacc5c1569c59e5782bf85
dennis0405 <dennis0405@snu.ac.kr>
2025-01-04 01:55:18 +0900

    User DTO

--- src/main/kotlin/com/example/toyTeam6Airbnb/user/controller/User.kt
+++ src/main/kotlin/com/example/toyTeam6Airbnb/user/controller/User.kt
@@ -1,3 +1,17 @@
 package com.example.toyTeam6Airbnb.user.controller
 
-class User
+data class User(
+    val id: String,
+    val username: String,
+    val kakaoNickname: String?,
+) {
+    companion object {
+        fun fromEntity(entity: com.example.toyTeam6Airbnb.user.persistence.UserEntity): User {
+            return User(
+                id = entity.id!!,
+                username = entity.username,
+                kakaoNickname = entity.kakaoNickname,
+            )
+        }
+    }
+}
