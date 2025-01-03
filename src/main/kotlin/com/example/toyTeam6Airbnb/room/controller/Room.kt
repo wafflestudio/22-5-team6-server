d4dd979108105ed97d205ccb1391a2cc92f17de0
dennis0405 <dennis0405@snu.ac.kr>
2025-01-04 00:29:26 +0900

    room Entity

--- src/main/kotlin/com/example/toyTeam6Airbnb/room/controller/Room.kt
+++ src/main/kotlin/com/example/toyTeam6Airbnb/room/controller/Room.kt
@@ -1,3 +1,43 @@
 package com.example.toyTeam6Airbnb.room.controller
 
-class Room
+import java.time.Instant
+
+data class Room(
+    val id: Long,
+    val hostId: Long,
+    val name: String,
+    val description: String?,
+    val type: String?,
+    val address: String,
+    val price: Double,
+    val maxOccupancy: Int,
+    val facilities: String?,
+    val interaction: String?,
+
+    // review, reservation의 id만 전달
+    val reservationIds: List<Long> = emptyList(),
+    val reviewIds: List<Long> = emptyList(),
+    val createdAt: Instant,
+    val updatedAt: Instant
+) {
+    companion object {
+        fun fromEntity(entity: com.example.toyTeam6Airbnb.room.persistence.RoomEntity): Room {
+            return Room(
+                id = entity.id!!,
+                hostId = entity.hostId,
+                name = entity.name,
+                description = entity.description,
+                type = entity.type,
+                address = entity.address,
+                price = entity.price,
+                maxOccupancy = entity.maxOccupancy,
+                facilities = entity.facilities,
+                interaction = entity.interaction,
+                reservationIds = entity.reservations.map { it.id!! },
+                reviewIds = entity.reviews.map { it.id!! },
+                createdAt = entity.createdAt,
+                updatedAt = entity.updatedAt
+            )
+        }
+    }
+}
--- src/main/kotlin/com/example/toyTeam6Airbnb/room/persistence/RoomEntity.kt
+++ src/main/kotlin/com/example/toyTeam6Airbnb/room/persistence/RoomEntity.kt
@@ -1,3 +1,46 @@
 package com.example.toyTeam6Airbnb.room.persistence
 
-class RoomEntity
+import com.example.toyTeam6Airbnb.reservation.persistence.ReservationEntity
+import com.example.toyTeam6Airbnb.review.persistence.ReviewEntity
+import jakarta.persistence.Column
+import jakarta.persistence.Id
+import jakarta.persistence.Entity
+import jakarta.persistence.GeneratedValue
+import jakarta.persistence.Table
+import jakarta.persistence.OneToMany
+import jakarta.persistence.GenerationType
+import java.time.Instant
+
+@Entity
+@Table(name = "rooms")
+class RoomEntity(
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    val id: Long? = null,
+    @Column(nullable = false)
+    var hostId: Long,
+    @Column(nullable = false)
+    val name: String,
+    @Column(columnDefinition = "TEXT")
+    var description: String?,
+    @Column
+    val type: String?,
+    @Column(columnDefinition = "TEXT", nullable = false)
+    var address: String,
+    @Column(nullable = false)
+    val price: Double,
+    @Column(nullable = false)
+    val maxOccupancy: Int,
+    @Column(columnDefinition = "TEXT")
+    var facilities: String?,
+    @Column(columnDefinition = "TEXT")
+    var interaction: String?,
+    @OneToMany(mappedBy = "room")
+    var reservations: List<ReservationEntity> = emptyList(),
+    @OneToMany(mappedBy = "room")
+    var reviews: List<ReviewEntity> = emptyList(),
+    @Column(nullable = false)
+    var createdAt: Instant,
+    @Column(nullable = false)
+    var updatedAt: Instant,
+)
