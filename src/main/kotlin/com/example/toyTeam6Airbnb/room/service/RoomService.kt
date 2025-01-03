daeb9f36b9944da1c5c523e3eb7645265edef7d2
dennis0405 <dennis0405@snu.ac.kr>
2025-01-04 01:55:46 +0900

    Room Service

--- src/main/kotlin/com/example/toyTeam6Airbnb/room/service/RoomService.kt
+++ src/main/kotlin/com/example/toyTeam6Airbnb/room/service/RoomService.kt
@@ -1,3 +1,30 @@
 package com.example.toyTeam6Airbnb.room.service
 
-interface RoomService
+import com.example.toyTeam6Airbnb.room.controller.Room
+
+interface RoomService{
+    fun addRoom(
+        name: String,
+        description: String,
+        type: String,
+        address: String,
+        price: Double,
+        maxOccupancy: Int,
+    ): Room
+
+    fun getRooms(
+
+    )
+
+    fun getRoomDetails(
+
+    )
+
+    fun updateRoom(
+
+    )
+
+    fun deleteRoom(
+
+    )
+}
--- src/main/kotlin/com/example/toyTeam6Airbnb/room/service/RoomServiceImpl.kt
+++ src/main/kotlin/com/example/toyTeam6Airbnb/room/service/RoomServiceImpl.kt
@@ -1,6 +1,38 @@
 package com.example.toyTeam6Airbnb.room.service
 
+import com.example.toyTeam6Airbnb.room.controller.Room
+import com.example.toyTeam6Airbnb.room.persistence.RoomRepository
 import org.springframework.stereotype.Service
 
 @Service
-class RoomServiceImpl : RoomService
+class RoomServiceImpl (
+    private val roomRepository: RoomRepository
+): RoomService {
+    override fun addRoom(
+        name: String,
+        description: String,
+        type: String,
+        address: String,
+        price: Double,
+        maxOccupancy: Int
+    ): Room {
+        TODO("Not yet implemented")
+    }
+
+    override fun getRooms() {
+        TODO("Not yet implemented")
+    }
+
+    override fun getRoomDetails() {
+        TODO("Not yet implemented")
+    }
+
+    override fun updateRoom() {
+        TODO("Not yet implemented")
+    }
+
+    override fun deleteRoom() {
+        TODO("Not yet implemented")
+    }
+
+}
