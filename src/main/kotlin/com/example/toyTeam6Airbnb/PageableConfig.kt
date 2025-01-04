6a526f0fcd8609dafaf7dd0ddf4daf047c4c7c91
dennis0405 <dennis0405@snu.ac.kr>
2025-01-04 15:23:22 +0900

    Paignation 기본 설정

---
+++ src/main/kotlin/com/example/toyTeam6Airbnb/PageableConfig.kt
@@ -0,0 +1,17 @@
+package com.example.toyTeam6Airbnb
+
+import org.springframework.context.annotation.Configuration
+import org.springframework.data.domain.PageRequest
+import org.springframework.data.web.PageableHandlerMethodArgumentResolver
+import org.springframework.web.method.support.HandlerMethodArgumentResolver
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
+
+@Configuration
+class PageableConfig : WebMvcConfigurer {
+    override fun addArgumentResolvers(resolvers: MutableList<HandlerMethodArgumentResolver>) {
+        val pageableResolver = PageableHandlerMethodArgumentResolver()
+        pageableResolver.setMaxPageSize(50) // 최대 페이지 크기 제한
+        pageableResolver.setFallbackPageable(PageRequest.of(0, 10)) // 기본 페이지 설정
+        resolvers.add(pageableResolver)
+    }
+}
