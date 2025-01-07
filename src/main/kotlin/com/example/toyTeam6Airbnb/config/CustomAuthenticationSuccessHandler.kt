fb2a40ca717a55625eea12d318217c2c5b30e68f
Seungmin Choi <seungmin960@gmail.com>
2025-01-07 16:52:53 +0900

    Allow test server to return bearer token directly

--- src/main/kotlin/com/example/toyTeam6Airbnb/config/CustomAuthenticationSuccessHandler.kt
+++ src/main/kotlin/com/example/toyTeam6Airbnb/config/CustomAuthenticationSuccessHandler.kt
@@ -3,13 +3,16 @@ package com.example.toyTeam6Airbnb.config
 import com.example.toyTeam6Airbnb.user.JwtTokenProvider
 import jakarta.servlet.http.HttpServletRequest
 import jakarta.servlet.http.HttpServletResponse
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.security.core.Authentication
 import org.springframework.security.web.authentication.SimpleUrlAuthenticationSuccessHandler
 import org.springframework.stereotype.Component
 
 @Component
 class CustomAuthenticationSuccessHandler(
-    private val jwtTokenProvider: JwtTokenProvider
+    private val jwtTokenProvider: JwtTokenProvider,
+    @Value("\${spring.profiles.active}")
+    private val profile: String
 ) : SimpleUrlAuthenticationSuccessHandler() {
 
     override fun onAuthenticationSuccess(
@@ -19,6 +22,6 @@ class CustomAuthenticationSuccessHandler(
     ) {
         val token = jwtTokenProvider.generateToken(authentication.name)
         response.addHeader("Authorization", "Bearer $token")
-        // response.sendRedirect("/redirect#token=$token")
+        if(profile == "dev") response.sendRedirect("/redirect#token=$token")
     }
 }
