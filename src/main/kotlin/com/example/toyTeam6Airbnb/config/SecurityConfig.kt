53841baba65a2760c41956739d7b918a922586bb
Seungmin Choi <seungmin960@gmail.com>
2025-01-09 02:45:28 +0900

    Added CORS config to securityconfig

--- src/main/kotlin/com/example/toyTeam6Airbnb/config/SecurityConfig.kt
+++ src/main/kotlin/com/example/toyTeam6Airbnb/config/SecurityConfig.kt
@@ -17,6 +17,9 @@ import org.springframework.security.crypto.password.PasswordEncoder
 import org.springframework.security.web.SecurityFilterChain
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter
 import org.springframework.security.web.authentication.logout.HttpStatusReturningLogoutSuccessHandler
+import org.springframework.web.cors.CorsConfiguration
+import org.springframework.web.cors.CorsConfigurationSource
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource
 
 @Configuration
 @EnableWebSecurity
@@ -40,8 +43,28 @@ class SecurityConfig(
     }
 
     @Bean
+    fun corsConfigurationSource(): CorsConfigurationSource {
+        val configuration = CorsConfiguration()
+        configuration.allowedOrigins = listOf("http://localhost:5173") // Vite
+        configuration.allowedMethods = listOf("GET", "POST", "PUT", "DELETE")
+        configuration.allowedHeaders = listOf(
+            "Origin",
+            "X-Requested-With",
+            "Content-Type",
+            "Authorization"
+        )
+        configuration.allowCredentials = true
+        configuration.maxAge = 3600L
+
+        val source = UrlBasedCorsConfigurationSource()
+        source.registerCorsConfiguration("/**", configuration)
+        return source
+    }
+
+    @Bean
     fun securityFilterChain(http: HttpSecurity): SecurityFilterChain {
         http {
+            cors { configurationSource = corsConfigurationSource() }
             csrf { disable() }
             authorizeHttpRequests {
                 authorize("/", permitAll)
