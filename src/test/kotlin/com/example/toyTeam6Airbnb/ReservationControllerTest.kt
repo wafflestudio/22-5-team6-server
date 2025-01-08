78334c55be1a3967b1e02504d65796c50dde9b40
Junbye <pjb0621@naver.com>
2025-01-09 02:07:11 +0900

    ReservationControllerTest 기본 기능에 대한 테스트 완료하였습니다.

--- src/test/kotlin/com/example/toyTeam6Airbnb/ReservationControllerTest.kt
+++ src/test/kotlin/com/example/toyTeam6Airbnb/ReservationControllerTest.kt
@@ -1,107 +1,38 @@
 package com.example.toyTeam6Airbnb
 
 import com.example.toyTeam6Airbnb.reservation.persistence.ReservationRepository
-import org.junit.jupiter.api.MethodOrderer
-import org.junit.jupiter.api.Order
+import org.json.JSONObject
+import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
-import org.junit.jupiter.api.TestMethodOrder
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.http.MediaType
 import org.springframework.test.web.servlet.MockMvc
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders
-import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers
-import org.springframework.test.web.servlet.result.MockMvcResultMatchers.status
 
 @SpringBootTest
 @AutoConfigureMockMvc
-@TestMethodOrder(MethodOrderer.OrderAnnotation::class)
 class ReservationControllerTest {
 
     @Autowired
     private lateinit var mockMvc: MockMvc
 
     @Autowired
+    private lateinit var dataGenerator: DataGenerator
+
+    @Autowired
     private lateinit var reservationRepository: ReservationRepository
 
-
     @Test
-    @Order(1)
-    fun `should register a new user`() {
+    fun `새로운 예약 생성시 201 응답 반환`() {
+        val (user, token) = dataGenerator.generateUserAndToken()
+        val room = dataGenerator.generateRoom(maxOccupancy = 10)
+        val roomId = room.id
         val requestBody = """
             {
-                "username": "testuser",
-                "password": "password123"
-            }
-        """.trimIndent()
-
-        val result = mockMvc.perform(
-            post("/api/auth/register")
-                .contentType(MediaType.APPLICATION_JSON)
-                .content(requestBody)
-        )
-            .andExpect(status().isOk)
-            .andReturn()
-
-        val response = result.response
-        // 상태 코드
-        println("Response Status: ${response.status}")
-
-        // 모든 헤더 출력
-        println("Response Headers:")
-        response.headerNames.forEach { headerName ->
-            println("$headerName: ${response.getHeader(headerName)}")
-        }
-
-        // 응답 본문
-        println("Response Body: ${response.contentAsString}")
-
-        // 요약 정보
-        println("Response Summary: ${response.toString()}")
-    }
-
-
-    @Test
-    @Order(2)
-    fun `should authenticate a user`() {
-        val requestBody = "username=testuser&password=password123"
-
-        val result = mockMvc.perform(
-            post("/api/auth/login")
-                .contentType(MediaType.APPLICATION_FORM_URLENCODED)
-                .content(requestBody)
-                .accept(MediaType.ALL)
-        )
-            .andExpect(status().isOk) // expect 200 status
-            .andReturn()
-
-
-        val response = result.response
-
-        // 상태 코드
-        println("Response Status: ${response.status}")
-
-        // 모든 헤더 출력
-        println("Response Headers:")
-        response.headerNames.forEach { headerName ->
-            println("$headerName: ${response.getHeader(headerName)}")
-        }
-
-        // 응답 본문
-        println("Response Body: ${response.contentAsString}")
-
-        // 요약 정보
-        println("Response Summary: ${response.toString()}")
-    }
-
-    @Test
-    @Order(3)
-    fun `should create a new reservation`() {
-        val requestBody = """
-            {
-                "roomId": 1,
+                "roomId": $roomId,
                 "startDate": "2023-12-01",
                 "endDate": "2023-12-10",
                 "numberOfGuests": 2
@@ -112,8 +43,9 @@ class ReservationControllerTest {
             MockMvcRequestBuilders.post("/api/v1/reservations")
                 .contentType(MediaType.APPLICATION_JSON)
                 .content(requestBody)
+                .header("Authorization", "Bearer $token")
         )
-            .andExpect(MockMvcResultMatchers.status().isOk) // expect 200 status
+            .andExpect(MockMvcResultMatchers.status().isCreated) // expect 201 status
             .andReturn()
 
         val responseContent = result.response.contentAsString
@@ -122,13 +54,34 @@ class ReservationControllerTest {
     }
 
     @Test
-    @Order(4)
-    fun `should get a specific reservation`() {
-        val reservationId = 1L // Assume this reservation exists in the database
+    fun `특정 ID의 예약 조회 성공시 200 응답 반환`() {
+        val (user, token) = dataGenerator.generateUserAndToken()
+        val room = dataGenerator.generateRoom(maxOccupancy = 10)
+        val roomId = room.id
+        val requestBody = """
+            {
+                "roomId": $roomId,
+                "startDate": "2023-12-01",
+                "endDate": "2023-12-10",
+                "numberOfGuests": 2
+            }
+        """.trimIndent()
+
+        val reservation = mockMvc.perform(
+            MockMvcRequestBuilders.post("/api/v1/reservations")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(requestBody)
+                .header("Authorization", "Bearer $token")
+        )
+            .andExpect(MockMvcResultMatchers.status().isCreated) // expect 201 status
+            .andReturn()
+
+        val reservationId = JSONObject(reservation.response.contentAsString).getLong("id")
 
         val result = mockMvc.perform(
             MockMvcRequestBuilders.get("/api/v1/reservations/$reservationId")
                 .accept(MediaType.APPLICATION_JSON)
+                .header("Authorization", "Bearer $token")
         )
             .andExpect(MockMvcResultMatchers.status().isOk) // expect 200 status
             .andReturn()
@@ -139,12 +92,33 @@ class ReservationControllerTest {
     }
 
     @Test
-    @Order(5)
-    fun `예약 수정하였을때 응답으로 200이 나와야한다`(){
-        val reservationId = 1L // Assume this reservation exists in the database
+    fun `예약 수정하였을때 응답으로 200이 나와야한다`() {
+        val (user, token) = dataGenerator.generateUserAndToken()
+        val room = dataGenerator.generateRoom(maxOccupancy = 10)
+        val roomId = room.id
         val requestBody = """
             {
-                "roomId": 1,
+                "roomId": $roomId,
+                "startDate": "2023-12-01",
+                "endDate": "2023-12-10",
+                "numberOfGuests": 2
+            }
+        """.trimIndent()
+
+        val reservation = mockMvc.perform(
+            MockMvcRequestBuilders.post("/api/v1/reservations")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(requestBody)
+                .header("Authorization", "Bearer $token")
+        )
+            .andExpect(MockMvcResultMatchers.status().isCreated) // expect 201 status
+            .andReturn()
+
+        val reservationId = JSONObject(reservation.response.contentAsString).getLong("id")
+
+        val updateRequestBody = """
+            {
+                "roomId": $roomId,
                 "startDate": "2023-12-01",
                 "endDate": "2024-01-05",
                 "numberOfGuests": 2
@@ -154,7 +128,8 @@ class ReservationControllerTest {
         val result = mockMvc.perform(
             MockMvcRequestBuilders.put("/api/v1/reservations/$reservationId")
                 .contentType(MediaType.APPLICATION_JSON)
-                .content(requestBody)
+                .content(updateRequestBody)
+                .header("Authorization", "Bearer $token")
         )
             .andExpect(MockMvcResultMatchers.status().isOk) // expect 200 status
             .andReturn()
@@ -165,6 +140,153 @@ class ReservationControllerTest {
     }
 
     @Test
+    fun `예약을 삭제한 경우 204 응답을 반환한다`() {
+        val (user, token) = dataGenerator.generateUserAndToken()
+        val room = dataGenerator.generateRoom(maxOccupancy = 10)
+        val roomId = room.id
+        val requestBody = """
+            {
+                "roomId": $roomId,
+                "startDate": "2023-12-01",
+                "endDate": "2023-12-10",
+                "numberOfGuests": 2
+            }
+        """.trimIndent()
 
+        val reservation = mockMvc.perform(
+            MockMvcRequestBuilders.post("/api/v1/reservations")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(requestBody)
+                .header("Authorization", "Bearer $token")
+        )
+            .andExpect(MockMvcResultMatchers.status().isCreated) // expect 201 status
+            .andReturn()
 
+        val reservationId = JSONObject(reservation.response.contentAsString).getLong("id")
+
+        val result = mockMvc.perform(
+            MockMvcRequestBuilders.delete("/api/v1/reservations/$reservationId")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(requestBody)
+                .header("Authorization", "Bearer $token")
+        )
+            .andExpect(MockMvcResultMatchers.status().isNoContent) // expect 204 status
+            .andReturn()
+    }
+
+    @Test
+    fun `전체 예약 조회시 200 응답을 반환한다`() {
+        val (user, token) = dataGenerator.generateUserAndToken()
+        val room = dataGenerator.generateRoom(maxOccupancy = 10)
+        val roomId = room.id
+        val requestBody = """
+            {
+                "roomId": $roomId,
+                "startDate": "2023-12-01",
+                "endDate": "2023-12-10",
+                "numberOfGuests": 2
+            }
+        """.trimIndent()
+
+        mockMvc.perform(
+            MockMvcRequestBuilders.post("/api/v1/reservations")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(requestBody)
+                .header("Authorization", "Bearer $token")
+        )
+            .andExpect(MockMvcResultMatchers.status().isCreated) // expect 201 status
+            .andReturn()
+
+        mockMvc.perform(
+            MockMvcRequestBuilders.post("/api/v1/reservations")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(
+                    """
+                    {
+                        "roomId": $roomId,
+                        "startDate": "2024-12-01",
+                        "endDate": "2024-12-10",
+                        "numberOfGuests": 2
+                    }
+                    """.trimIndent()
+                )
+                .header("Authorization", "Bearer $token")
+        )
+            .andExpect(MockMvcResultMatchers.status().isCreated) // expect 201 status
+            .andReturn()
+
+        val result = mockMvc.perform(
+            MockMvcRequestBuilders.get("/api/v1/reservations")
+                .accept(MediaType.APPLICATION_JSON)
+                .header("Authorization", "Bearer $token")
+        )
+            .andExpect(MockMvcResultMatchers.status().isOk) // expect 200 status
+            .andReturn()
+
+        val responseContent = result.response.contentAsString
+        // Add assertions to verify the response content if needed
+        println(responseContent)
+    }
+
+    @Test
+    fun `예약에대한 Availablity를 200을 반환한다`() {
+        val (user, token) = dataGenerator.generateUserAndToken()
+        val room = dataGenerator.generateRoom(maxOccupancy = 10)
+        val roomId = room.id
+
+        mockMvc.perform(
+            MockMvcRequestBuilders.post("/api/v1/reservations")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(
+                    """
+            {
+                "roomId": $roomId,
+                "startDate": "2025-01-05",
+                "endDate": "2025-01-10",
+                "numberOfGuests": 2
+            }
+                    """.trimIndent()
+                )
+                .header("Authorization", "Bearer $token")
+        )
+            .andExpect(MockMvcResultMatchers.status().isCreated) // expect 201 status
+            .andReturn()
+
+        mockMvc.perform(
+            MockMvcRequestBuilders.post("/api/v1/reservations")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(
+                    """
+                    {
+                        "roomId": $roomId,
+                        "startDate": "2025-01-15",
+                        "endDate": "2025-01-20",
+                        "numberOfGuests": 2
+                    }
+                    """.trimIndent()
+                )
+                .header("Authorization", "Bearer $token")
+        )
+            .andExpect(MockMvcResultMatchers.status().isCreated) // expect 201 status
+            .andReturn()
+
+        val result = mockMvc.perform(
+            MockMvcRequestBuilders.get("/api/v1/reservations/availability/$roomId")
+                .param("year", "2025")
+                .param("month", "1")
+                .accept(MediaType.APPLICATION_JSON)
+                .header("Authorization", "Bearer $token")
+        )
+            .andExpect(MockMvcResultMatchers.status().isOk) // expect 200 status
+            .andReturn()
+
+        val responseContent = result.response.contentAsString
+        // Add assertions to verify the response content if needed
+        println(responseContent)
+    }
+
+    @BeforeEach
+    fun setUp() {
+        dataGenerator.clearAll()
+    }
 }
