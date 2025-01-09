aadcc522583c76c4679e267db8e0fc44ec2a5741
dennis0405 <dennis0405@snu.ac.kr>
2025-01-09 15:52:47 +0900

    Config 설정

--- src/main/kotlin/com/example/toyTeam6Airbnb/PageableConfig.kt
+++ src/main/kotlin/com/example/toyTeam6Airbnb/PageableConfig.kt
@@ -10,7 +10,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
 class PageableConfig : WebMvcConfigurer {
     override fun addArgumentResolvers(resolvers: MutableList<HandlerMethodArgumentResolver>) {
         val pageableResolver = PageableHandlerMethodArgumentResolver()
-        pageableResolver.setMaxPageSize(50) // 최대 페이지 크기 제한
+        pageableResolver.setMaxPageSize(100) // 최대 페이지 크기 제한
         pageableResolver.setFallbackPageable(PageRequest.of(0, 10)) // 기본 페이지 설정
         resolvers.add(pageableResolver)
     }
