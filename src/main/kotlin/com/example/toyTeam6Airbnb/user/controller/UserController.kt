e4c1a5035d637d63a3eadf1883df1a1e7e5d6a8c
Seungmin Choi <seungmin960@gmail.com>
2025-01-11 19:19:06 +0900

    Add endpoint for swagger auth testing

--- src/main/kotlin/com/example/toyTeam6Airbnb/user/controller/UserController.kt
+++ src/main/kotlin/com/example/toyTeam6Airbnb/user/controller/UserController.kt
@@ -7,33 +7,41 @@ import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
-import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
-@RequestMapping("/api")
 @Tag(name = "User Controller", description = "User Controller API")
 class UserController(
     private val userService: UserService
 ) {
 
-    @PostMapping("/v1/ping")
+    @PostMapping("/api/v1/ping")
     @Operation(summary = "Ping pong", description = "Sample ping pong api for testing")
     fun ping(): ResponseEntity<String> {
         return ResponseEntity.ok("pong")
     }
 
-    @GetMapping("/v1/ping")
+    @GetMapping("/api/v1/ping")
     fun getPing(): ResponseEntity<String> {
         return ResponseEntity.ok("pong")
     }
 
-    @PostMapping("/auth/register")
+    @PostMapping("/api/auth/register")
     fun register(
         @RequestBody request: RegisterRequest
     ): ResponseEntity<Unit> {
         userService.register(username = request.username, password = request.password)
         return ResponseEntity.ok().build()
+    }
+
+    // a mapping just for swagger testing
+    // token parameter is passed as a query parameter
+    // just return the token parameter in body
+    @Operation(summary = "Redirect", description = "Redirect to the token", hidden = true)
+    @GetMapping("/redirect")
+    fun redirect(@RequestParam token: String): ResponseEntity<String> {
+        return ResponseEntity.ok(token)
     }
 }
 
