8ffd2fdb026b0f8bb2a31a8fa3cd14374d6515d3
dennis0405 <dennis0405@snu.ac.kr>
2025-01-16 17:10:57 +0900

    dto 구조 변화

--- src/main/kotlin/com/example/toyTeam6Airbnb/reservation/controller/Reservation.kt
+++ src/main/kotlin/com/example/toyTeam6Airbnb/reservation/controller/Reservation.kt
@@ -1,58 +1,61 @@
 package com.example.toyTeam6Airbnb.reservation.controller
 
 import com.example.toyTeam6Airbnb.reservation.persistence.ReservationEntity
-import java.time.Instant
 import java.time.LocalDate
 
 data class Reservation(
-    val id: Long,
-    val userId: Long,
-    val roomId: Long,
-    val reviewId: Long?,
-    val startDate: LocalDate,
-    val endDate: LocalDate,
-    val totalPrice: Double,
-    val numberOfGuests: Int,
-    val createdAt: Instant,
-    val updatedAt: Instant
+    val reservationId: Long,
 ) {
     companion object {
         fun fromEntity(entity: ReservationEntity): Reservation {
             return Reservation(
-                id = entity.id!!,
+                reservationId = entity.id!!,
+            )
+        }
+    }
+}
+
+data class ReservationDetails(
+    val reservationId: Long,
+    val userId: Long,
+    val roomId: Long,
+    val startDate: LocalDate,
+    val endDate: LocalDate,
+    val place: String,
+    val numberOfGuests: Int,
+    //val imageUrl: String,
+) {
+    companion object {
+        fun fromEntity(entity: ReservationEntity): ReservationDetails {
+            return ReservationDetails(
+                reservationId = entity.id!!,
                 userId = entity.user.id!!,
                 roomId = entity.room.id!!,
-                reviewId = entity.review?.id,
                 startDate = entity.startDate,
                 endDate = entity.endDate,
-                totalPrice = entity.totalPrice,
-                createdAt = entity.createdAt,
-                updatedAt = entity.updatedAt,
-                numberOfGuests = entity.numberOfGuests
+                place = entity.room.address.sido,
+                numberOfGuests = entity.numberOfGuests,
+                //imageUrl = entity.room.imageUrl
             )
         }
     }
 }
 
 data class ReservationDTO(
-    val id: Long,
-    val userId: Long,
-    val roomId: Long,
+    val reservationId: Long,
+    val place: String,
     val startDate: LocalDate,
     val endDate: LocalDate,
-    val place: String,
-    val numberOfGuests: Int
+    //val imageUrl: String,
 ) {
     companion object {
         fun fromEntity(entity: ReservationEntity): ReservationDTO {
             return ReservationDTO(
-                id = entity.id!!,
-                userId = entity.user.id!!,
-                roomId = entity.room.id!!,
+                reservationId = entity.id!!,
+                place = entity.room.address.sido,
                 startDate = entity.startDate,
                 endDate = entity.endDate,
-                place = entity.room.address.sido,
-                numberOfGuests = entity.numberOfGuests
+                //imageUrl = entity.room.imageUrl
             )
         }
     }
