7f1622490cdc213f4773e4204d11e2fc77626a28
Junbye <pjb0621@naver.com>
2025-01-18 18:07:17 +0900

    유저 회원가입 시 프로필 이미지 업로드 Url 제공

--- src/main/kotlin/com/example/toyTeam6Airbnb/user/service/UserService.kt
+++ src/main/kotlin/com/example/toyTeam6Airbnb/user/service/UserService.kt
@@ -6,8 +6,9 @@ import com.example.toyTeam6Airbnb.user.controller.User
 interface UserService {
     fun register(
         request: RegisterRequest
-    ): User?
+    ): Pair<User?, String>
 
+    // register 결과로 <User, Url> 페어를 반환하도록 수정
     fun hasProfile(
         username: String
     ): Boolean
