89aec0a2500921538c58f09eaa74b02d81f0e89c
dennis0405 <dennis0405@snu.ac.kr>
2025-01-27 16:21:11 +0900

    paignation validation 분할

--- src/main/kotlin/com/example/toyTeam6Airbnb/ValidatePageable.kt
+++ src/main/kotlin/com/example/toyTeam6Airbnb/ValidatePageable.kt
@@ -5,28 +5,31 @@ import org.springframework.data.domain.Pageable
 import org.springframework.http.HttpStatus
 import org.springframework.http.HttpStatusCode
 
-fun validatePageable(pageable: Pageable): Pageable {
-    val allowedSortProperties = listOf("createdAt", "id", "name", "price")
-    pageable.sort.forEach { order ->
-        if (order.property !in allowedSortProperties) {
-            throw WrongSortingException()
+fun validateSort(pageable: Pageable, allowedSortProperties: List<String>) {
+    if (pageable.sort.isSorted) {
+        pageable.sort.forEach { order ->
+            if (order.property !in allowedSortProperties) {
+                throw WrongSortingException()
+            }
         }
     }
-
-    return PageRequest.of(
-        pageable.pageNumber,
-        pageable.pageSize,
-        pageable.sort
-    )
 }
 
-fun validateSortedPageable(pageable: Pageable): Pageable {
+fun validatePageableForRoom(pageable: Pageable): Pageable {
+    val allowedSortProperties = listOf("createdAt", "id", "name", "price.perNight")
+    validateSort(pageable, allowedSortProperties)
+    return PageRequest.of(pageable.pageNumber, pageable.pageSize, pageable.sort)
+}
+
+fun validatePageableForReview(pageable: Pageable): Pageable {
     val allowedSortProperties = listOf("createdAt", "rating")
-    pageable.sort.forEach { order ->
-        if (order.property !in allowedSortProperties) {
-            throw WrongSortingException()
-        }
-    }
+    validateSort(pageable, allowedSortProperties)
+    return PageRequest.of(pageable.pageNumber, pageable.pageSize, pageable.sort)
+}
+
+fun validatePageableForReservation(pageable: Pageable): Pageable {
+    val allowedSortProperties = listOf("createdAt", "startDate", "endDate")
+    validateSort(pageable, allowedSortProperties)
     return PageRequest.of(pageable.pageNumber, pageable.pageSize, pageable.sort)
 }
 
@@ -42,4 +45,3 @@ class WrongSortingException : PageableException(
     httpStatusCode = HttpStatus.BAD_REQUEST,
     msg = "Wrong form of sorting"
 )
-// 이후에 rating, review, price 등의 정렬 기준이 추가될 경우 함수를 추가하여 처리할 수 있습니다.
