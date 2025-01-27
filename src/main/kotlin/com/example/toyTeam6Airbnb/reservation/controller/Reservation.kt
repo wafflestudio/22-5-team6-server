75a97f798d5a0df90a8d7987f3d24d9fedf5f55a
dennis0405 <dennis0405@snu.ac.kr>
2025-01-27 13:53:46 +0900

    Response DTO 변경

--- src/main/kotlin/com/example/toyTeam6Airbnb/reservation/controller/Reservation.kt
+++ src/main/kotlin/com/example/toyTeam6Airbnb/reservation/controller/Reservation.kt
@@ -1,6 +1,7 @@
 package com.example.toyTeam6Airbnb.reservation.controller
 
 import com.example.toyTeam6Airbnb.reservation.persistence.ReservationEntity
+import com.example.toyTeam6Airbnb.room.persistence.Price
 import java.time.LocalDate
 
 data class Reservation(
@@ -18,9 +19,11 @@ data class Reservation(
 data class ReservationDetails(
     val reservationId: Long,
     val roomId: Long,
+    val roomName: String,
     val startDate: LocalDate,
     val endDate: LocalDate,
     val place: String,
+    val price: Double,
     val numberOfGuests: Int,
     val imageUrl: String // 대표이미지 다운로드 URL
 ) {
@@ -29,9 +32,11 @@ data class ReservationDetails(
             return ReservationDetails(
                 reservationId = entity.id!!,
                 roomId = entity.room.id!!,
+                roomName = entity.room.name,
                 startDate = entity.startDate,
                 endDate = entity.endDate,
                 place = entity.room.address.sido,
+                price = entity.room.price.total, // 숙소 가격
                 numberOfGuests = entity.numberOfGuests,
                 imageUrl = imageUrl // 대표이미지 다운로드 URL
             )
@@ -50,7 +55,7 @@ data class ReservationDTO(
         fun fromEntity(entity: ReservationEntity, imageUrl: String): ReservationDTO {
             return ReservationDTO(
                 reservationId = entity.id!!,
-                place = entity.room.address.sido,
+                place = entity.room.address.sigungu,
                 startDate = entity.startDate,
                 endDate = entity.endDate,
                 imageUrl = imageUrl // 대표이미지 다운로드 url, 이렇게하는 이유가 Reservation entity에 연결된 Room은 Entity인데 이거에 연결되있는건 Image엔티티들이라 Url을 가져오기 위해 service를 또 불러야만함
