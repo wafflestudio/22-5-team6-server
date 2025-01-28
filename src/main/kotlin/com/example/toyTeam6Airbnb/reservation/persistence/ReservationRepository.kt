752849ea493b1ebb42233c16e612b3ce249a6a64
dennis0405 <dennis0405@snu.ac.kr>
2025-01-29 01:40:21 +0900

    주석 삭제

--- src/main/kotlin/com/example/toyTeam6Airbnb/reservation/persistence/ReservationRepository.kt
+++ src/main/kotlin/com/example/toyTeam6Airbnb/reservation/persistence/ReservationRepository.kt
@@ -8,12 +8,17 @@ import org.springframework.data.domain.Pageable
 import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.data.jpa.repository.Lock
 import org.springframework.data.jpa.repository.Query
+import org.springframework.data.repository.query.Param
+import java.time.LocalDate
 
 interface ReservationRepository : JpaRepository<ReservationEntity, Long> {
 
     @Lock(LockModeType.PESSIMISTIC_WRITE)
     @Query("SELECT r FROM ReservationEntity r WHERE r.id = :id")
     fun findByIdOrNullForUpdate(id: Long): ReservationEntity?
+
+    @Query("SELECT r FROM ReservationEntity r WHERE r.startDate <= :endDate AND r.endDate >= :startDate")
+    fun findReservationsByDateRange(@Param("startDate") startDate: LocalDate, @Param("endDate") endDate: LocalDate): List<ReservationEntity>
 
     fun findAllByRoom(room: RoomEntity): List<ReservationEntity>
 
