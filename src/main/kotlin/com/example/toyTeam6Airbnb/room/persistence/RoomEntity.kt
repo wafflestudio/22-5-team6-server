4a7a6ad82d0b9337713e0e8c724d9e7f2ceee536
dennis0405 <dennis0405@snu.ac.kr>
2025-01-30 18:43:54 +0900

    linter

--- src/main/kotlin/com/example/toyTeam6Airbnb/room/persistence/RoomEntity.kt
+++ src/main/kotlin/com/example/toyTeam6Airbnb/room/persistence/RoomEntity.kt
@@ -4,7 +4,20 @@ import com.example.toyTeam6Airbnb.image.persistence.ImageEntity
 import com.example.toyTeam6Airbnb.reservation.persistence.ReservationEntity
 import com.example.toyTeam6Airbnb.review.persistence.ReviewEntity
 import com.example.toyTeam6Airbnb.user.persistence.UserEntity
-import jakarta.persistence.*
+import jakarta.persistence.Column
+import jakarta.persistence.Embedded
+import jakarta.persistence.Entity
+import jakarta.persistence.FetchType
+import jakarta.persistence.GeneratedValue
+import jakarta.persistence.GenerationType
+import jakarta.persistence.Id
+import jakarta.persistence.JoinColumn
+import jakarta.persistence.ManyToOne
+import jakarta.persistence.OneToMany
+import jakarta.persistence.PrePersist
+import jakarta.persistence.PreUpdate
+import jakarta.persistence.Table
+import jakarta.persistence.UniqueConstraint
 import java.time.Instant
 
 @Entity
