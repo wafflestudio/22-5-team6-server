8e7b78f4abd5c89afde9b0968551b1c7766c0aad
dennis0405 <dennis0405@snu.ac.kr>
2025-01-31 21:36:53 +0900

    Refactoring

--- src/main/kotlin/com/example/toyTeam6Airbnb/PageableConfig.kt
+++ src/main/kotlin/com/example/toyTeam6Airbnb/PageableConfig.kt
@@ -1,7 +1,6 @@
 package com.example.toyTeam6Airbnb
 
 import org.springframework.context.annotation.Configuration
-import org.springframework.data.domain.PageRequest
 import org.springframework.data.web.PageableHandlerMethodArgumentResolver
 import org.springframework.web.method.support.HandlerMethodArgumentResolver
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
@@ -11,7 +10,6 @@ class PageableConfig : WebMvcConfigurer {
     override fun addArgumentResolvers(resolvers: MutableList<HandlerMethodArgumentResolver>) {
         val pageableResolver = PageableHandlerMethodArgumentResolver()
         pageableResolver.setMaxPageSize(100) // 최대 페이지 크기 제한
-        pageableResolver.setFallbackPageable(PageRequest.of(0, 10)) // 기본 페이지 설정
         resolvers.add(pageableResolver)
     }
 }
